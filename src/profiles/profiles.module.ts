f35074a508b68733ea3c6809e3ffb99abaec63bb
Pierre Heidinger <pierre.heidinger@gmail.com>
2021-05-04 18:53:52 -0300

    experiences

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -3,14 +3,20 @@ import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProfilesController } from './profiles.controller';
+import { ExperiencesController } from './experience/experience.controller';
 import { Profile, ProfileSchema } from './profiles.schema';
 import { ProfileService } from './profiles.service';
+import { ExperienceService } from './experience/experience.service';
+import { Experience, ExperienceSchema } from './experience/experience.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema }]),
+    MongooseModule.forFeature([
+      { name: Profile.name, schema: ProfileSchema },
+      { name: Experience.name, schema: ExperienceSchema }
+    ]),
   ],
-  controllers: [ProfilesController],
-  providers: [ProfileService],
+  controllers: [ProfilesController,ExperiencesController],
+  providers: [ProfileService,ExperienceService],
 })
 export class ProfilesModule {}
